d481c9aa3453012f920ed1e0ea80852798fdc6e1
unknown <abdullahimohamedyusuf04@gmail.com>
2023-06-15 13:10:12 +0200

    login fixed

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, task,deletePage,create_user
+from .views import home, task,deletePage,login,signup
 
 urlpatterns = [
     path('', home, name="home"),
     path('task', task, name="task"),
     path('delete-page/<str:pk>', deletePage, name="delete"),
-    path('create-user', create_user, name="create")
+    path('login', login, name="login"),
+    path('create-user', signup, name="create")
 ]
--- base/views.py
+++ base/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .form import Task, SignUp
 from .models import Todo
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth import login as auth_login
 
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required(login_url='/login')
 def home(request):
     form = Task
     context = {"form": form}
@@ -15,14 +22,14 @@ def home(request):
             return redirect('/task')
     return render(request, 'base/index.html', context)
 
-
+@login_required(login_url='/login')
 def task(request):
     todo = Todo.objects.all()
     context= {'todo': todo}
     return render(request, 'base/tasks.html', context)
 
 
-
+@login_required(login_url='/login')
 def deletePage(request, pk):
     room = Todo.objects.get(id=pk)
     context = {"room": room}
@@ -32,7 +39,43 @@ def deletePage(request, pk):
         return redirect('/task')
     return render(request, 'base/delete.html', context)
 
-def create_user(request):
-    creation = SignUp
-    context = {"form": creation}
-    return render(request, 'base/user.html', context)
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username, password=password)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(username=username,password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect('')
+        else:
+            messages.error(request, 'Username or password does not exist')
+
+        
+
+    return render(request, 'base/user.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+
+
+        if User.objects.filter(username=username, password=password).exists():
+            messages.error(request, 'User already exists')
+            return redirect('/login')
+        else:
+            User.objects.create(username=username,password=password,email=email)
+            messages.success(request, 'User created')
+            return redirect('/login')
+
+        
+    return render(request, 'base/sign.html')
